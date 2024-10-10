b13fd721ac38d03ec2422f97615feaab86423fdd
VikashPR <vikashrajendran2806@gmail.com>
2024-10-10 13:46:21 +0000

    Add initial bootstrap script for marketing module

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom";
+
+// Mount function to start up the app
+const mount = (el) => {
+    ReactDOM.render(
+        <h1>Hi, there!</h1>,
+        el
+    )
+};
