3f0a2aa58a3717a978e2c884d8d3522a7471de84
VikashPR <vikashrajendran2806@gmail.com>
2024-10-10 13:50:27 +0000

    Refactor bootstrap script to dynamically mount marketing module

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -8,3 +8,17 @@ const mount = (el) => {
         el
     )
 };
+
+// if we are in development and in isolation, call mount immediately
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector("#_marketing-dev-root")
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+// if we are running through container and we should export the mount function
+
+export {mount}
